f5594b83c93d6a0339ddb285727c680bf20d6e0b
Martin Olivesten <marol450@student.liu.se>
2024-04-02 17:09:02 +0200

    Add RefChain to misc

--- araceae/misc.py
+++ araceae/misc.py
@@ -1,5 +1,6 @@
 """A collection of miscellaneous utilities."""
 import numpy as np
+from typing import TypeVar, Generic
 
 
 def clamp(v: float, _min: float, _max: float) -> float:
@@ -41,3 +42,65 @@ class iota():
 
 
 auto = iota()
+
+
+_T2 = TypeVar('_T2')
+
+
+class RefChain(Generic[_T2]):
+    _value: _T2 | None
+    _next: 'RefChain[_T2] | None'
+    _end: bool
+
+    def __init__(self, value: '_T2 | RefChain[_T2]') -> None:
+        if isinstance(value, RefChain):
+            self._value = None
+            self._next = value
+            self._end = False
+        else:
+            self._value = value
+            self._next = None
+            self._end = True
+
+    def set(self, value: _T2) -> None:
+        if self._end:
+            assert self._value
+            self._value = value
+        else:
+            assert self._next
+            self._next.set(value)
+
+    def get(self) -> _T2:
+        if self._end:
+            assert self._value
+            return self._value
+        assert self._next
+        return self._next.get()
+
+    def is_end(self) -> bool:
+        return self._end
+
+    def get_end(self) -> 'RefChain[_T2]':
+        c = self
+        while True:
+            if c._end: break
+            assert c._next
+            c = c._next
+        return c
+
+    def move_end(self, next: 'RefChain[_T2]') -> None:
+        assert self is next
+        end = self.get_end()
+        end._value = None
+        end._next = next
+        end._end = False
+
+    def __str__(self) -> str:
+        if self._end:
+            assert self._value
+            return self._value.__str__()
+        assert self._next
+        return f'{id(self)} -> {self._next.__str__()}'
+
+    def __repr__(self) -> str:
+        return self.__str__()
