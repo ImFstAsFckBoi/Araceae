253e533ab049f47ce86fea2c979932b58c2b553d
Martin Olivesten <marol450@student.liu.se>
2024-04-14 23:16:10 +0200

    Extended Result type

--- araceae/result.py
+++ araceae/result.py
@@ -1,35 +1,82 @@
 from typing import TypeVar, Generic, Optional, Protocol
+from typing_extensions import Never
 
 
-T = TypeVar('T', covariant=True)
+T = TypeVar("T", covariant=True)
 
 
 class Result(Generic[T], Protocol):
+    """Result type which could be either Ok or Err"""
+
     @property
     def ok(self) -> bool: ...
 
+    def or_raise(
+        self,
+        exc: Exception | type[Exception] = ...,  # noqa: E501
+    ) -> T:
+        """Get the value or raise the `exc` exception if the Result is Err"""
+        ...
 
-class Err:
-    cause: str
-    ok = False
-
-    __match_args__ = ('cause',)
-
-    def __init__(self, cause: Optional[str] = None):
-        self.cause = cause or ''
-
-    @property
-    def value(self) -> None:
-        raise TypeError('Attempted to grab `value` from object of type `Err`. '
-                        'Check that result is type `Ok` using `match`, '
-                        '`isinstance` or `Result.ok` first.')
+    def or_none(self) -> T | None:
+        """Get the value or None is the Result is Err"""
+        ...
 
 
-class Ok(Generic[T]):
+class Ok(Result, Generic[T]):
+    """Positive result, containing a valid result value"""
+
     value: T
-    ok = True
 
-    __match_args__ = ('value',)
+    __match_args__ = ("value",)
 
     def __init__(self, val: T):
         self.value = val
+
+    @property
+    def ok(self):
+        return True
+
+    def or_raise(self, exc=...) -> T:
+        return self.value
+
+    def or_none(self) -> T | None:
+        return self.value
+
+
+class Err(Result):
+    """Negative result, not containing a valid result value"""
+
+    cause: str
+
+    __match_args__ = ("cause",)
+
+    def __init__(self, cause: Optional[str] = None):
+        self.cause = cause or ""
+
+    @property
+    def ok(self):
+        return False
+
+    @property
+    def value(self) -> None:
+        raise TypeError(
+            "Attempted to grab `value` from object of type `Err`. "
+            "Check that result is type `Ok` using `match`, "
+            "`isinstance` or `Result.ok` first."
+        )
+
+    def or_raise(
+        self, exc=TypeError("'or_raise' was called on Result with 'Err' type.")
+    ) -> Never:
+        raise exc
+
+    def or_none(self) -> None:
+        return None
+
+
+def or_err(value: Optional[T]) -> Ok[T] | Err:
+    if value is None:
+        return Err("Value was None")
+    else:
+        return Ok(value)
--- tests/test_result.py
+++ tests/test_result.py
@@ -1,5 +1,10 @@
-from araceae.result import Result, Ok, Err
 from pytest import raises
+from araceae.result import Result, Ok, Err, or_err
+from typing import runtime_checkable, Protocol
+
+
+@runtime_checkable
+class _checkable_result(Result, Protocol): ...
 
 
 def get_ok() -> Result[int]:
@@ -11,21 +16,51 @@ def get_err() -> Result[int]:
 
 
 def test_result() -> None:
-    x = get_ok()
+    ok = get_ok()
+    err = get_err()
 
-    match x:
+    match ok:
         case Ok(v):
-            assert x.ok
+            assert ok.ok
             assert v == 100
         case Err(c):
             raise Exception(c)
 
-    y = get_err()
-
-    match y:
+    match err:
         case Ok(_):
             assert False, "This was unexpected"
         case Err(c):
-            assert not y.ok
+            assert not err.ok
             with raises(TypeError):
-                print(y.value)
+                print(err.value)
+
+
+def test_proto():
+    ok = get_ok()
+    err = get_err()
+
+    assert isinstance(ok, _checkable_result)
+    assert isinstance(ok, Ok)
+    assert not isinstance(ok, Err)
+    assert isinstance(err, _checkable_result)
+    assert isinstance(err, Err)
+    assert not isinstance(err, Ok)
+
+
+def test_methods_and_functions():
+    ok = get_ok()
+    err = get_err()
+
+    assert ok.or_raise() == 100
+
+    with raises(Exception):
+        err.or_raise()
+
+    with raises(MemoryError):
+        err.or_raise(MemoryError("Oh oh"))
+
+    assert ok.or_none() is not None
+    assert err.or_none() is None
+
+    assert not or_err(None).ok
+    assert or_err(123).ok
